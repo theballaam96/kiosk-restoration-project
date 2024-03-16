df63a0787a514122c6bcd5139995ad023ce80a02
Tom Ballaam <theballaam96@gmail.com>
2024-03-15 22:51:36 -0500

    Initial texture remap pass

---
+++ build/calculate_mapping.py
@@ -0,0 +1,66 @@
+import json
+import zlib
+
+def fast_similarity_score(byte_array1, byte_array2):
+    if len(byte_array1) != len(byte_array2):
+        return 0
+
+    length = len(byte_array1)
+    if length == 0:
+        return 1.0
+
+    matching_bytes = sum(1 for b1, b2 in zip(byte_array1, byte_array2) if b1 == b2)
+    return matching_bytes / length
+
+data = [
+    {"name": "kiosk", "sha": [], "offset": 0x1A7C20, "table": 24},
+    {"name": "us", "sha": [], "offset": 0x101C50, "table": 25},
+]
+
+mapping = {}
+
+for item in data:
+    with open(f"../rom/dk64_{item['name']}.z64", "rb") as fh:
+        fh.seek(item["offset"] + (item["table"] << 2) + (32 << 2))
+        count = int.from_bytes(fh.read(4), "big")
+        fh.seek(item["offset"] + (item["table"] << 2))
+        table_start = item["offset"] + int.from_bytes(fh.read(4), "big")
+        for x in range(count):
+            fh.seek(table_start + (x << 2))
+            file_start = item["offset"] + (int.from_bytes(fh.read(4), "big") & 0x7FFFFFFF)
+            file_end = item["offset"] + (int.from_bytes(fh.read(4), "big") & 0x7FFFFFFF)
+            file_size = file_end - file_start
+            if file_size <= 0:
+                continue
+            fh.seek(file_start)
+            compressed_data = zlib.decompress(fh.read(file_size), (15 + 32))
+            item["sha"].append(compressed_data)
+
+SIMILARITY_THRESHOLD = 0.9
+
+for xi, x in enumerate(data[1]["sha"]):
+    index = 0
+    set_match = False
+    max_mapping_score = 0
+    max_mapping_item = 0
+    while True:
+        score = fast_similarity_score(bytearray(x), bytearray(data[0]["sha"][index]))
+        if score == 1:
+            mapping[xi] = index
+            break
+        elif score > max_mapping_score:
+            if score >= SIMILARITY_THRESHOLD:
+                mapping[xi] = index
+                set_match = True
+            max_mapping_score = score
+            max_mapping_item = index
+        index += 1
+        if index >= len(data[0]["sha"]):
+            if not set_match:
+                print("Unmatched", xi, "Max Score:", max_mapping_score,"(",max_mapping_item,")")
+            break
+
+with open("texture_mapping.json", "w") as fh:
+    fh.write(json.dumps(mapping, indent=4))
+
+
--- build/geometry_mapper.py
+++ build/geometry_mapper.py
@@ -1,3 +1,5 @@
+import json
+
 def geo_us_to_kiosk(file_name: str):
     start_data = None
     end_data = None
@@ -17,3 +19,20 @@ def geo_us_to_kiosk(file_name: str):
             fh.seek(0x28 + (4 * x))
             new = old - 0x8
             fh.write(new.to_bytes(4, "big"))
+        # Texture re-mapping
+        fh.seek(0x2C)
+        start_pointer = int.from_bytes(fh.read(4), "big")
+        end_pointer = int.from_bytes(fh.read(4), "big")
+        dl_count = int((end_pointer - start_pointer) / 8)
+        f = open("./build/texture_mapping.json")
+        mapping_data = json.load(f)
+        for x in range(dl_count):
+            fh.seek(start_pointer + (x * 8))
+            command = int.from_bytes(fh.read(1), "big")
+            if command != 0xFD:
+                continue
+            fh.seek(start_pointer + (x * 8) + 4)
+            tex_idx = int.from_bytes(fh.read(4), "big")
+            if str(tex_idx) in mapping_data:
+                fh.seek(start_pointer + (x * 8) + 4)
+                fh.write((mapping_data[str(tex_idx)]).to_bytes(4, "big"))
